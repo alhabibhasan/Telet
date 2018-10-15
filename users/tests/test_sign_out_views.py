78d720747833e245d28ec46862ce3363d6286502
Muhammed Hasan <Muhammedahasan@outlook.com>
2018-10-15 21:06:19 +0100

    Add tests for sign out view

--- users/tests/test_sign_out_views.py
+++ users/tests/test_sign_out_views.py
@@ -6,7 +6,6 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse_lazy
 
-from users.forms import UserLoginForm
 from users.models import Teler
 
 
@@ -24,84 +23,25 @@ class TestPatientSignIn(TestCase):
                                             )
         test_teler_1.save()
 
-
     def test_sign_in_valid_details(self):
         data = {
             'username': 'telet@test1.com',
             'password': '12345'
         }
 
-        response = self.client.post(reverse_lazy('users:login'), data=data)
+        response_sign_in = self.client.post(reverse_lazy('users:signin'), data=data)
 
         user = auth.get_user(self.client)
 
         self.assertEqual(user.username, data['username'])
         self.assertTrue(user.is_authenticated)
 
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/users/logged-in/')
+        self.assertEqual(response_sign_in.status_code, 302)
+        self.assertRedirects(response_sign_in, '/users/signed-in/')
 
-
-    def test_sign_in_invalid_user_credentials(self):
-        data = {
-            'username': 'telet@test1.com',
-            'password': '54321'
-        }
-
-        response = self.client.post(reverse_lazy('users:login'), data=data)
+        response_sign_out = self.client.get(reverse_lazy('users:signout'))
 
         user = auth.get_user(self.client)
 
-        self.assertNotEqual(user.username, data['username'])
+        self.assertEqual(response_sign_out.status_code, 200)
         self.assertFalse(user.is_authenticated)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'login.html')
-        self.assertContains(response, 'Please enter a correct username and '
-                                      'password. Note that both fields may be '
-                                      'case-sensitive.')
-
-    def test_sign_in_missing_user_credentials_1(self):
-        data = {
-            'username': '',
-            'password': '54321'
-        }
-
-        response = self.client.post(reverse_lazy('users:login'), data=data)
-
-        user = auth.get_user(self.client)
-
-        self.assertFalse(user.is_authenticated)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'login.html')
-
-    def test_sign_in_missing_user_credentials_2(self):
-        data = {
-            'username': '',
-            'password': ''
-        }
-
-        response = self.client.post(reverse_lazy('users:login'), data=data)
-
-        user = auth.get_user(self.client)
-
-        self.assertFalse(user.is_authenticated)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'login.html')
-
-    def test_sign_in_invalid_user_credentials_2(self):
-        data = {
-            'username': '345345345345',
-            'password': '345345346545'
-        }
-
-        response = self.client.post(reverse_lazy('users:login'), data=data)
-
-        user = auth.get_user(self.client)
-
-        self.assertFalse(user.is_authenticated)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'login.html')
