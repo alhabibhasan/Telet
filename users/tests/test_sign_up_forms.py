e8c2f133deea50c77bd52f88a6bdbeeb9001a800
Muhammed Hasan <Muhammedahasan@outlook.com>
2018-10-15 20:26:47 +0100

    Add tests for user sign up

--- users/tests/test_sign_up_forms.py
+++ users/tests/test_sign_up_forms.py
@@ -1,91 +1,98 @@
-# Create your tests here.
-from django.urls import reverse_lazy
-from rest_framework import status
-from rest_framework.test import APIRequestFactory, APITestCase
-from rest_framework.utils import json
+from django.test import TestCase
 
-from users.views import CreateUserView
+from users.forms import UserSignUpForm
 
 
-class TestPatientSignUp(APITestCase):
-    # Init the factory object
-    factory = APIRequestFactory()
-    # Get the user sign up url
-    url = reverse_lazy('user-signup')
-
+class TestPatientSignUp(TestCase):
     def test_sign_up_normal(self):
-        """
-        Ensure that we can create an account
-        """
-        # Define the data to be used
-        data = {
-            "user": {
-                "first_name": "Test",
-                "last_name": "User",
-                "email": "test_user@telet.com",
-                "password1": "password",
-                "password2": "password"
-            },
-            "mobile_number": "07940236488",
-            "date_of_birth": "2000-01-01",
-            "gender": "M"
-        }
 
-        # define the request
-        request = self.factory.post(path=self.url, data=json.dumps(data), content_type='application/json')
+        form = UserSignUpForm(data={
+            'email': 'test@telet.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'gender': 'M',
+            'date_of_birth': '01/01/2000',
+            'mobile_number': '07940236488',
+            'password1': 'random_password_123',
+            'password2': 'random_password_123'
+        })
 
-        # init the view
-        view = CreateUserView.as_view()
+        self.assertTrue(form.is_valid())
 
-        # send the request to the view and store the response
-        response = view(request)
 
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+    def test_sign_up_password_mismatch(self):
 
-    def test_sign_up_duplicate_email(self):
-        """
-        Ensure that we can't create an account with an email that is already being used
-        """
-        data = {
-            "user": {
-                "first_name": "Test",
-                "last_name": "User",
-                "email": "test_user@telet.com",
-                "password1": "password",
-                "password2": "password"
-            },
-            "mobile_number": "07940236488",
-            "date_of_birth": "2000-01-01",
-            "gender": "M"
-        }
-        view = CreateUserView.as_view()
+        form = UserSignUpForm(data={
+            'email': 'test@telet.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'gender': 'M',
+            'date_of_birth': '01/01/2000',
+            'mobile_number': '07940236488',
+            'password1': 'random_password_123',
+            'password2': 'random_password_456'
+        })
 
-        first_request = self.factory.post(path=self.url, data=json.dumps(data), content_type='application/json')
-        view(first_request)  # send the first request
+        self.assertFalse(form.is_valid())
 
-        second_request = self.factory.post(path=self.url, data=json.dumps(data), content_type='application/json')
-        second_response = view(second_request)  # send the second request with the same data value
+    def test_sign_up_invalid_email(self):
 
-        self.assertEquals(second_response.status_code, status.HTTP_400_BAD_REQUEST)
+        form = UserSignUpForm(data={
+            'email': 'test',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'gender': 'M',
+            'date_of_birth': '01/01/2000',
+            'mobile_number': '07940236488',
+            'password1': 'random_password_123',
+            'password2': 'random_password_123'
+        })
 
-    def test_sign_up_mismatch_passwords(self):
-        """
-        Ensure that we can't create an account with mismatching passwords
-        """
-        data = {
-            "user": {
-                "first_name": "Test",
-                "last_name": "User",
-                "email": "test_user@telet.com",
-                "password1": "password",
-                "password2": "password1"
-            },
-            "mobile_number": "07940236488",
-            "date_of_birth": "2000-01-01",
-            "gender": "M"
-        }
-        request = self.factory.post(path=self.url, data=json.dumps(data), content_type='application/json')
-        view = CreateUserView.as_view()
-        response = view(request)
+        self.assertFalse(form.is_valid())
 
-        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_sign_up_invalid_DOB(self):
+        form = UserSignUpForm(data={
+            'email': 'test@telet.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'gender': 'M',
+            'date_of_birth': '01/01/1',
+            'mobile_number': '07940236488',
+            'password1': 'random_password_123',
+            'password2': 'random_password_123'
+        })
+
+        self.assertFalse(form.is_valid())
+
+    def test_sign_up_invalid_gender(self):
+        form = UserSignUpForm(data={
+            'email': 'test@telet.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'gender': 'XXX',
+            'date_of_birth': '01/01/2000',
+            'mobile_number': '07940236488',
+            'password1': 'random_password_123',
+            'password2': 'random_password_123'
+        })
+
+        self.assertFalse(form.is_valid())
+
+    def test_sign_up_email_exists(self):
+        from users.models import CustomUser
+        user = CustomUser(email='test@telet.com', username='test@telet.com')
+        user.set_password('random_password_123')
+        user.save()
+
+        form = UserSignUpForm(data={
+            'email': 'test@telet.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'gender': 'F',
+            'date_of_birth': '01/01/2000',
+            'mobile_number': '07940236488',
+            'password1': 'random_password_123',
+            'password2': 'random_password_123'
+        })
+
+        self.assertFalse(form.is_valid())
